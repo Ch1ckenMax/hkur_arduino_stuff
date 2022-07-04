a78220396656500d027013a6ab9995970ed3b361
Ch1ckenMax <59310578+Ch1ckenMax@users.noreply.github.com>
2022-07-04 10:47:33 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # hkur_arduino_stuff
-Library: <br>
-https://github.com/DFRobot/DFRobot_MCP2515
-
+http://adafruit.github.io/Adafruit-GFX-Library/html/index.html<br>
+https://wiki.dfrobot.com/CAN-BUS_Shield_V2__SKU__DFR0370_#target_1<br>
+https://github.com/prenticedavid/MCUFRIEND_kbv
--- master_board.ino
+++ master_board.ino
@@ -1,24 +1,41 @@
-#include <DFRobot_MCP2515.h>
+ // demo: CAN-BUS Shield, send data
+  #include <df_can.h>
+  #include <SPI.h>
+  //COM5
 
-  DFRobot_MCP2515 CAN(10);                                  // Set CS pin
+  const int SPI_CS_PIN = 10;
+
+  MCPCAN CAN(SPI_CS_PIN);                                    // Set CS pin
 
   void setup()
   {
       Serial.begin(115200);
-      while( ! CAN.begin(CAN_500KBPS) ){   // init can bus : baudrate = 500k
-        Serial.println("DFROBOT's CAN BUS Shield init fail");
-        Serial.println("Please Init CAN BUS Shield again");
-        delay(3000);
-      }
-      Serial.println("DFROBOT's CAN BUS Shield init ok!\n");
+      int count = 50;                                     // the max numbers of initializint the CAN-BUS, if initialize failed first!.
+      do {
+          CAN.init();   //must initialize the Can interface here!
+          if(CAN_OK == CAN.begin(CAN_500KBPS))                   // init can bus : baudrate = 500k
+          {
+              Serial.println("DFROBOT's CAN BUS Shield init ok!");
+              break;
+          }
+          else
+          {
+              Serial.println("DFROBOT's CAN BUS Shield init fail");
+              Serial.println("Please Init CAN BUS Shield again");
+
+              delay(100);
+              if (count <= 1)
+                  Serial.println("Please give up trying!, trying is useless!");
+          }
+
+      }while(count--);
 
 }
   const int inputPIN = A0;
-  uint8_t throttlePercentage[1] = {5};
+  INT8U throttlePercentage[1] = {5};
   void loop()
     {
         throttlePercentage[0] = analogRead(inputPIN);
         CAN.sendMsgBuf(0x06, 0, 1, throttlePercentage);
-        Serial.println(throttlePercentage[0]);
         delay(1000);                       // send data per 100ms
     }
