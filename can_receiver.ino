43105b58b3b1d41f28118dfed24d2e1577a57d1c
JOKAKEASHI <91318926+JOKAKEASHI@users.noreply.github.com>
2022-07-04 11:43:12 +0800

    Update and rename can_receiver_and_display.ino to can_receiver.ino

---
+++ can_receiver.ino
@@ -0,0 +1,55 @@
+#include <SPI.h>
+#include "df_can.h"
+//COM4
+
+const int SPI_CS_PIN = 10;
+MCPCAN CAN(SPI_CS_PIN);                                    // Set CS pin
+
+void setup()
+{
+  Serial.begin(115200);
+  int count = 50;                                     // the max numbers of initializint the CAN-BUS, if initialize failed first!.
+  do {
+    CAN.init();   //must initialize the Can interface here!
+    if (CAN_OK == CAN.begin(CAN_500KBPS))                  // init can bus : baudrate = 500k
+    {
+      Serial.println("DFROBOT's CAN BUS Shield init ok!");
+      break;
+    }
+    else
+    {
+      Serial.println("DFROBOT's CAN BUS Shield init fail");
+      Serial.println("Please Init CAN BUS Shield again");
+
+      delay(100);
+      if (count <= 1)
+        Serial.println("Please give up trying!, trying is useless!");
+    }
+
+  } while (count--);
+
+}
+
+INT8U buf[2] = {0,0};
+INT8U len = 2;
+
+void loop()
+{
+  // CAN Bus stuff
+  if (CAN_MSGAVAIL == CAN.checkReceive())           // check if data coming
+  {
+    CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+    for (int i = 0; i < len; i++)   // print the data
+    {
+      Serial.print((int) buf[i]);
+      Serial.print("\t");
+
+      delay(250);
+    }
+    Serial.println();
+  }
+  else {
+    Serial.print("NO SIGNAL");
+    delay(1000);
+  }
+}
